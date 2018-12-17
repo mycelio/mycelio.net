0925178c0cc3a51ffc2fd1f650b4b420fe5db55b
Ilja <iljapanic@gmail.com>
2018-12-17 11:29:50 +0100

    basic ScrollMagic integration

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,35 @@
-import fullpage from 'fullpage.js';
+(function($) {
+
+	// get window height
+	var windowHeight = window.innerHeight;
+
+	// init ScrollMagic
+	var controller = new ScrollMagic.Controller({
+		globalSceneOptions: {
+				triggerHook: 'onLeave'
+		}
+	});
+
+	// attach a scene to each section
+	$('section').each(function() {
+		var name = $(this).attr('id');
+
+		new ScrollMagic.Scene({
+			triggerElement: this
+		})
+		.setPin(this)
+		.addIndicators({name: name})
+		.addTo(controller);
+	})
+
+	// add active class to each sections
+	new ScrollMagic.Scene({
+		offset: windowHeight*3
+	})
+	.setClassToggle('section#four', 'is-active')
+	.addTo(controller);
+
+})(jQuery);
 
 
-new fullpage('#fullpage', {
-	licenseKey: process.env.FULLPAGE_LICENSE,
-	navigation: false,
-	navigationPosition: 'left',
-	// autoScrolling: false
-});
 
-//methods
-// fullpage_api.setAllowScrolling(false);
